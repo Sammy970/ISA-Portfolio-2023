8d55f8a24350672fcf036b730a939dc741c375e9
Parag <paragpurandare50@gmail.com>
2023-08-28 13:35:42 +0530

    done with iv with responsiveness and the modal opening

--- src/components/IVCardPage/IVCardPage.js
+++ src/components/IVCardPage/IVCardPage.js
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   Card,
   CardBody,
@@ -5,46 +6,83 @@ import {
   Divider,
   Image,
   Text,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
 } from "@chakra-ui/react";
-import React from "react";
 
-const IVCardPage = ({ variant }) => {
+
+const IVCardPage = ({ variant, desc }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [imageSrc, setImageSrc] = React.useState(""); // State to hold image source
+
+  const openModal = (src) => {
+    setImageSrc(src);
+    onOpen();
+  };
+
+  const getImageSrc = (variant) => {
+    return `iv_${variant}_text.png`; // Adjust the image file naming pattern
+  };
+
+ 
+
   return (
-    <Card
-      maxW={"full"}
-      h={"full"}
-      borderRadius={0}
-      border={"2px solid black"}
-      boxShadow={"8px -8px 0px #0f0f0f"}
-    >
-      <CardHeader
-        p={1}
-        fontSize={15}
-        color={"#000000"}
-        fontFamily="'Press Start 2P', cursive"
-        ml={2}
+    <>
+      <Card
+        maxW="full"
+        h="full"
+        borderRadius={0}
+        border="2px solid black"
+        boxShadow="8px -8px 0px #0f0f0f"
+        cursor="pointer"
+        onClick={() => openModal(getImageSrc(variant))}
+        _hover={{
+          transform: "scale(1.05)", // Increase scale on hover
+          boxShadow: "4px -4px 1px #0f1f0f", // Adjust box shadow on hover
+          transition: "transform 0.3s, box-shadow 0.3s", // Add smooth transition
+        }}
       >
-        <Text>{variant}</Text>
-      </CardHeader>
-      <Divider />
-      <CardBody p={2} textAlign={"center"}>
-        {variant === "1" && (
-          <Image src="iv_one_text.png" w={{ base: "auto", md: "100%" }} />
-        )}
-        {variant === "2" && (
-          <Image src="iv_two_text.png" w={{ base: "auto", md: "100%" }} />
-        )}
-        {variant === "3" && (
-          <Image src="iv_three_text.png" w={{ base: "auto", md: "100%" }} />
-        )}
-        {variant === "4" && (
-          <Image src="iv_four_text.png" w={{ base: "auto", md: "100%" }} />
-        )}
-        {variant === "5" && (
-          <Image src="iv_five_text.png" w={{ base: "auto", md: "100%" }} />
-        )}
-      </CardBody>
-    </Card>
+        <CardHeader
+          p={1}
+          fontSize={15}
+          color={"#000000"}
+          fontFamily="'Press Start 2P', cursive"
+          ml={2}
+        >
+          <Text>{`${variant}`}</Text>
+        </CardHeader>
+        <Divider />
+        <CardBody p={2} textAlign={"center"} mt={2}>
+          <Image src={getImageSrc(variant)} w={{ base: "auto", md: "100%" }} />
+        </CardBody>
+      </Card>
+      <Modal isOpen={isOpen} onClose={onClose} size="xl" isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{`${variant}`}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Image margin={0} src={imageSrc} alt={`Variant ${variant}`} />
+            <Text fontSize="24" lineHeight="tall" fontFamily="body">
+              {desc}
+            </Text>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">More</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
--- src/pages/IV.js
+++ src/pages/IV.js
@@ -1,47 +1,90 @@
 import React from "react";
-import { Box, Grid, GridItem, Image, Text } from "@chakra-ui/react";
+import { Box, Center, Grid, GridItem, Image, Text } from "@chakra-ui/react";
 import IVCardPage from "../components/IVCardPage/IVCardPage";
 
 const IV = () => {
-  return(
-    <>
-  <Box borderRadius="13px" color="#0f0f0f" mb={10} ml={20} pl={20} >
-  <Text ml={3} mb={3} fontSize="25px" fontFamily="google-sans-black">
-    ISA Insustrial Visits
-  </Text>
-  <Grid templateColumns={{ base: "auto", md: "4fr 4fr"}} gap={10}>
-    <GridItem boxShadow="2xl" maxW={{ base: "50%", md: "auto" }}>
-      <IVCardPage variant={"1"} />
-    </GridItem>
-    <GridItem boxShadow="1xl" maxW={{ base: "50%", md: "auto" }}>
-    <Image src="image 1.png" w={{ base: "50%", md: "auto" }} />
-    </GridItem>
-    <GridItem boxShadow="1xl" maxW={{ base: "50%", md: "auto" }}>
-    <Image src="image 2.png" w={{ base: "50%", md: "auto" }} />
-    </GridItem>
-    <GridItem boxShadow="2xl" maxW={{ base: "50%", md: "auto" }}>
-      <IVCardPage variant={"2"} />
-    </GridItem>
-    <GridItem boxShadow="2xl" maxW={{ base: "50%", md: "auto" }}>
-      <IVCardPage variant={"3"} />
-    </GridItem>
-    <GridItem boxShadow="1xl" maxW={{ base: "50%", md: "auto" }}>
-    <Image src="image 3.png" w={{ base: "50%", md: "auto" }} />
-    </GridItem>
-    <GridItem boxShadow="1xl" maxW={{ base: "50%", md: "auto" }}>
-    <Image src="image 4.png" w={{ base: "50%", md: "auto" }} />
-    </GridItem>
-    <GridItem boxShadow="2xl" maxW={{ base: "50%", md: "auto" }}>
-      <IVCardPage variant={"4"} />
-    </GridItem>
-    <GridItem boxShadow="2xl" maxW={{ base: "50%", md: "auto" }}>
-      <IVCardPage variant={"5"} />
-    </GridItem>
-
-  </Grid>
-</Box>
-</>
-  )
+  return (
+    <Box borderRadius="13px" color="#0f0f0f" mx={["10px", "20px"]} mb={10}>
+      <Center>
+      <Text mx="auto" mb={10} fontSize="25px" fontFamily="google-sans-black">
+        ISA Industrial Visits
+      </Text>
+      </Center>
+      <Center>
+      <Grid
+        templateColumns={{ base: "1fr", md: "repeat(2, 1fr)" }}
+        gap={28}
+        maxW="80%" // Set maximum width for the entire grid
+        marginX="auto" // Center align the grid horizontally
+      >
+        <Box
+          boxShadow="2xl"
+          width="100%" // Full width for mobile, otherwise auto
+          maxWidth={{ md: "100%" }} // Maximum width for larger screens
+        >
+          <IVCardPage variant={"1"} desc={"situated in Pune and founded in 2006, is a well-known producer and provider of Control Panel Cabinets. During an industry visit, students learnt about PLC panels, hardware, installation, and safety procedures, gaining important insights into PLC commissioning and technological advancement."} />
+        </Box>
+        <Box
+          boxShadow="1xl"
+          width="100%" // Full width for mobile, otherwise auto
+          maxWidth={{ md: "100%" }} // Maximum width for larger screen10
+        >
+          <Image src="image 1.png" w="100%" />
+        </Box>
+        <Box
+          boxShadow="1xl"
+          width="100%" // Full width for mobile, otherwise auto
+          maxWidth={{ md: "100%" }} // Maximum width for larger screens
+        >
+          <Image src="image 2.png" w="100%" />
+        </Box>
+        <Box
+          boxShadow="2xl"
+          width="100%" // Full width for mobile, otherwise auto
+          maxWidth={{ md: "100%" }} // Maximum width for larger screens
+        >
+          <IVCardPage variant={"2"} desc={"On September 28, 2019, ISA Pune attended Automation expo 2019, which was hosted at the Bombay Convention and Exhibition Centre in Goregaon, Mumbai. "}/>
+        </Box>
+        <Box
+          boxShadow="2xl"
+          width="100%" // Full width for mobile, otherwise auto
+          maxWidth={{ md: "100%" }} // Maximum width for larger screens
+        >
+          <IVCardPage variant={"3"} desc={"VIT ISA Pune organised an industrial visit to ISRO Ahmedabad from April 25, 2022 to May 5, 2022. Senior ISRO scientists are available for interaction with the visitors during this visit."} />
+        </Box>
+        <Box
+          boxShadow="1xl"
+          width="100%" // Full width for mobile, otherwise auto
+          maxWidth={{ md: "100%" }} // Maximum width for larger screens
+        >
+          <Image src="image 3.png" w="100%" />
+        </Box>
+        <Box
+          boxShadow="1xl"
+          width="100%" // Full width for mobile, otherwise auto
+          maxWidth={{ md: "100%" }} // Maximum width for larger screens
+        >
+          <Image src="image 4.png" w="100%" />
+        </Box>
+        <Box
+          boxShadow="2xl"
+          width="100%" // Full width for mobile, otherwise auto
+          maxWidth={{ md: "100%" }} // Maximum width for larger screens
+        >
+          <IVCardPage variant={"4"}  desc={"Endress+Hauser Flowtec (India) Pvt. Ltd. was the subject of an industrial visit scheduled by ISA on the 20th of March 2019. Professor Kapil Mundada of the Instrumentation Engineering Department organised the visit. Instruments for flow measurement are custom-made by Endress+Hauser."}/>
+        </Box>
+        <Box
+          boxShadow="2xl"
+          width="100%" // Full width for mobile, otherwise auto
+          maxWidth={{ md: "100%" }} // Maximum width for larger screens
+        >
+          <IVCardPage variant={"5"}  />
+        </Box>
+        {/* ... Repeat the pattern for other items */}
+      </Grid>
+      </Center>
+    </Box>
+  );
 };
 
 export default IV;
