3b25eb9d5d0b8c1e481d2c9d4880f29041df098d
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-09-01 11:26:29 +0530

    Added for more testing

--- api/hello.js
+++ api/hello.js
@@ -4,5 +4,11 @@ const client = new MongoClient(process.env.REACT_APP_MONGO_URL);
 
 export default async function handler(req, res) {
   // console.log(client);
-  res.status(200).json({ message: client });
+
+  await client.connect();
+  const db = await client.db("countDB");
+  const collection = await db.collection("countCollection");
+  const data = collection.find({}).toArray;
+
+  res.status(200).json({ message: data });
 }
